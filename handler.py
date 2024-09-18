0121b69534e1942066ad18b5186191e33b383541
Danil1994 <danilaleksandrovich36@gmail.com>
2024-09-18 10:52:07 +0300

    create handler function, configuratw roures

---
+++ handler.py
@@ -0,0 +1,41 @@
+import json
+from datetime import datetime
+
+
+def read(path: str) -> dict:
+    with open(path, 'r') as file:
+        data = json.load(file)
+    return data
+
+
+def filter_by_time(benchmarking_results: list, start_time: str, end_time: str) -> (list, str):
+    if end_time < start_time:
+        return "End time should be less than start time"
+    start_time = datetime.fromisoformat(start_time)
+    end_time = datetime.fromisoformat(end_time)
+
+    # Filters data
+    filtered_results = [
+        result for result in benchmarking_results
+        if start_time <= datetime.fromisoformat(result['timestamp']) <= end_time
+    ]
+
+    return filtered_results
+
+
+def get_result(benchmarking_results: list) -> dict:
+    # calculate the average performance
+    total_entries = len(benchmarking_results)
+    average_token_count = sum(d['token_count'] for d in benchmarking_results) / total_entries
+    average_time_to_first_token = sum(d['time_to_first_token'] for d in benchmarking_results) / total_entries
+    average_time_per_output_token = sum(d['time_per_output_token'] for d in benchmarking_results) / total_entries
+    average_total_generation_time = sum(d['total_generation_time'] for d in benchmarking_results) / total_entries
+
+    average_results = {
+        "average_token_count": average_token_count,
+        "average_time_to_first_token": average_time_to_first_token,
+        "average_time_per_output_token": average_time_per_output_token,
+        "average_total_generation_time": average_total_generation_time
+    }
+
+    return (average_results)
--- main.py
+++ main.py
@@ -1,16 +1,22 @@
-from typing import Union
-
 from fastapi import FastAPI
-from datetime import datetime
+
+from handler import get_result, filter_by_time, read
 
 app = FastAPI()
 
 
 @app.get("/results/average")
 def get_average_performance():
-    pass
+    data = read('test_database.json')
+    benchmarking_results = data['benchmarking_results']
+    result = get_result(benchmarking_results)
+    return result
 
 
 @app.get("/results/average/{start_time}/{end_time}")
-def get_average_performance_within_time(start_time:datetime, end_time:datetime):
-    pass
+def get_average_performance_within_time(start_time: str, end_time: str):
+    data = read('test_database.json')
+    benchmarking_results = data['benchmarking_results']
+    filter_results = filter_by_time(benchmarking_results, start_time, end_time)
+    result = get_result(filter_results)
+    return result
